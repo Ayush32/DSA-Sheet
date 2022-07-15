97841e559052157562cd6fb58d08f505be01ea14
Ayush Gupta <721ayush@gmail.com>
2022-07-15 16:27:15 +0530

    Create find_duplicate_number.cpp

---
+++ Array/find_duplicate_number.cpp
@@ -0,0 +1,24 @@
+1st - brute force;
+2nd  = hashmap
+
+//3rd approach flyod cycle algo or same as detect loop in linkedlist
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        fast = nums[0];
+        while(slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
