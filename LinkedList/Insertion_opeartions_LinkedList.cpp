657f300a33e6954d1c65c8143e5d2f8cda7c8c97
Ayush Gupta <721ayush@gmail.com>
2022-08-06 18:30:27 +0530

    Update Insertion_opeartions_LinkedList.cpp

--- LinkedList/Insertion_opeartions_LinkedList.cpp
+++ LinkedList/Insertion_opeartions_LinkedList.cpp
@@ -31,11 +31,59 @@ void insertAtHead(Node* &head,int x){
   head = temp;
 }
 
+void insertAtMiddle(Node* &head, Node* tail, int posiiton, int x){
+  
+//   create new node to with x data
+  
+  Node* temp = head;
+//   Node* tailtemp = tail
+  Node* newNode = new Node(x);
+  int count = 1;
+  
+//   if the position is 1st or head 
+  if(position == 1)
+  {
+    newNode ->next = head;
+    head  = newNode;
+    return;
+  }
+  while(count < positiom - 1){
+    temp = temp->next;
+    count++;
+    
+//     if the position is last or tail position 
+    if(temp - > next == NULL){
+      tail->next = newNode;
+      tail = newNode;
+      return ;
+    }
+  // insert at middle or any psoition
+    newNode -> next = temp;
+    temp->next = newNode;
+    
+    
+}
+// Insertion at the end or tail
+
+void insertAtTail(Node* &tail,int x){
+  
+//   create new node to with x data
+  
+  Node* temp = new Node(x);
+  // make new node next as old node data address
+  tail->next = temp;
+  head = temp;
+}
+
 int main() {
   Node* node1 = new Node(10);
   cout << node1 ->data;
   // head pointed to the node1
   Node *head = node1;
+  Node * tail = node1;
+  insertAtHead(head,12);
+  insertAtEnd(tail,15);
+  
   
   
   insertAtHead(head,12);
